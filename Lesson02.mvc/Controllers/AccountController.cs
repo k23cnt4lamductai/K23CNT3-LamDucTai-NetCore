4000e2534742d2224900bb176a807f21d8223996
unknown <lamvietung1979@gmail.com>
2025-05-09 16:02:31 +0700

    Day2&3

---
+++ Lesson02.mvc/Controllers/AccountController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Lesson02.mvc.Models;
+
+namespace Lesson02.mvc.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Account> accounts = new List<Account>
+            {
+                new Account()
+                {
+                    Id = 1,
+                    Name = "Hoàng Anh",
+                    Email = "anh@gmail.com",
+                    Phone = "0338962005",
+                    Address = "Hà Nội",
+                    Avatar = Url.Content("~/Avatar/02.jfif"),
+                    Gender = 1,
+                    Bio = "My name is small",
+                    Birthday = new DateTime(1998, 7, 15)
+                },
+                new Account(), // ...
+                new Account() // ...
+
+            };
+            
+            return View();
+        }
+    }
+}
