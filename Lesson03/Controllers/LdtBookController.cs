3a82b8024aa655f0c78174ebef84704f787d8c06
unknown <lamvietung1979@gmail.com>
2025-05-14 11:04:51 +0700

    Day4

---
+++ Lesson03/Controllers/LdtBookController.cs
@@ -0,0 +1,63 @@
+﻿using Lesson04.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lesson04.Controllers
+{
+    public class LdtBookController : Controller
+    {
+        List<LdtBook> ldtBook = new List<LdtBook>
+{
+    new LdtBook
+    {
+        LdtID = "B001",
+        LdtTitle = "Cái gì đấy ",
+        LdtDesciption = "A comprehensive guide to learning C# for beginners.",
+        LdtImage = "/images/1.jpg",
+        LdtPrice = "19.99",
+        LdtPage = "320"
+    },
+    new LdtBook
+    {
+        LdtID = "B002",
+        LdtTitle = "My Hero Academy",
+        LdtDesciption = "Advanced concepts and real-world projects with ASP.NET Core.",
+        LdtImage = "/images/2.jpg",
+        LdtPrice = "29.99",
+        LdtPage = "450"
+    },
+    new LdtBook
+    {
+        LdtID = "B003",
+        LdtTitle = "jujutsu kaisen",
+        LdtDesciption = "Learn data access in .NET using Entity Framework Core.",
+        LdtImage = "/images/3.jpg",
+        LdtPrice = "24.99",
+        LdtPage = "380"
+    },
+    new LdtBook
+    {
+        LdtID = "B004",
+        LdtTitle = "attack on titan",
+        LdtDesciption = ".",
+        LdtImage = "/images/4.jpg",
+        LdtPrice = "22.50",
+        LdtPage = "310"
+    },
+    new LdtBook
+    {
+        LdtID = "B005",
+        LdtTitle = "Dragon Balls Super",
+        LdtDesciption = ".",
+        LdtImage = "/images/5.jpg",
+        LdtPrice = "27.99",
+        LdtPage = "400"
+    }
+};
+
+
+        public IActionResult LdtIndex()
+        {
+            return View(ldtBook);
+        }
+    }
+}
---
+++ Lesson03/Models/LdtBook.cs
@@ -0,0 +1,13 @@
+﻿namespace Lesson04.Models
+{
+    public class LdtBook
+    {
+        public string LdtID { get; set; }
+        public string LdtTitle { get; set; }
+        public string LdtDesciption { get; set; }
+        public string LdtImage { get; set; }
+        public string LdtPrice { get; set; }
+        public string LdtPage { get; set; }
+
+    }
+}
