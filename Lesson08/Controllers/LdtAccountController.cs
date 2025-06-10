303039973e0899b24a6853c72c5a04f242c36e3f
unknown <lamvietung1979@gmail.com>
2025-06-10 11:34:25 +0700

    Day8

---
+++ Lesson08/Controllers/LdtAccountController.cs
@@ -0,0 +1,158 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Principal;
+
+namespace Lesson08.Controllers
+{
+    public class LdtAccountController : Controller
+    {
+        public class LdtAccountController : Controller
+        {
+            private static List<LdtAccount> ldtListAccount = new List<LdtAccount>()
+            {
+            new LdtAccount
+                {
+                    LdtId = 230022113,
+                    LdtFullName = "Lâm Đức Tài",
+                    LdtEmail = "lamvietung1979@gmail.com",
+                    LdtPhone = "0974326194",
+                    LdtAddress = "Lớp K23CNT3",
+                    LdtAvatar = "1.jpg",
+                    LdtBirthday = new DateTime(2005, 09, 26),
+                    LdtGender = "Nam",
+                    LdtPassword = "123456789",
+                    LdtFacebook = "https://www.facebook.com/lam.uc.tai.444516"
+                },
+                new LdtAccount
+                {
+                    LdtId = 2,
+                    LdtFullName = "Trần Thị B",
+                    LdtEmail = "tranthib@example.com",
+                    LdtPhone = "0987654321",
+                    LdtAddress = "456 Đường B, Quận 3, TP.HCM",
+                    LdtAvatar = "avatar2.jpg",
+                    LdtBirthday = new DateTime(1992, 8, 15),
+                    LdtGender = "Nữ",
+                    LdtPassword = "password2",
+                    LdtFacebook = "https://facebook.com/tranthib"
+                },
+                new LdtAccount
+                {
+                    LdtId = 3,
+                    LdtFullName = "Lê Văn C",
+                    LdtEmail = "levanc@example.com",
+                    LdtPhone = "0911222333",
+                    LdtAddress = "789 Đường C, Quận 5, TP.HCM",
+                    LdtAvatar = "avatar3.jpg",
+                    LdtBirthday = new DateTime(1988, 12, 1),
+                    LdtGender = "Nam",
+                    LdtPassword = "password3",
+                    LdtFacebook = "https://facebook.com/levanc"
+                },
+                new LdtAccount
+                {
+                    LdtId = 4,
+                    LdtFullName = "Phạm Thị D",
+                    LdtEmail = "phamthid@example.com",
+                    LdtPhone = "0909876543",
+                    LdtAddress = "321 Đường D, Quận 7, TP.HCM",
+                    LdtAvatar = "avatar4.jpg",
+                    LdtBirthday = new DateTime(1995, 3, 10),
+                    LdtGender = "Nữ",
+                    LdtPassword = "password4",
+                    LdtFacebook = "https://facebook.com/phamthid"
+                },
+                new LdtAccount
+                {
+                    LdtId = 5,
+                    LdtFullName = "Hoàng Văn E",
+                    LdtEmail = "hoangvane@example.com",
+                    LdtPhone = "0933444555",
+                    LdtAddress = "654 Đường E, Quận 10, TP.HCM",
+                    LdtAvatar = "avatar5.jpg",
+                    LdtBirthday = new DateTime(1991, 7, 22),
+                    LdtGender = "Nam",
+                    LdtPassword = "password5",
+                    LdtFacebook = "https://facebook.com/hoangvane"
+                }
+             };
+
+
+
+
+            // GET: LdtAccountController
+            public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: LdtAccountController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: LdtAccountController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: LdtAccountController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: LdtAccountController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: LdtAccountController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: LdtAccountController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: LdtAccountController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Lesson08/Models/LdtAccount.cs
@@ -0,0 +1,53 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lesson08.Models
+{
+    public class LdtAccount
+    {
+        [Key]
+        [Display(Name = "Mã")]
+        public int LdtId { get; set; }
+
+        [Display(Name = "Họ và tên")]
+        [Required(ErrorMessage = "Họ và tên không được để trống")]
+        [MinLength(6, ErrorMessage = "Họ tên ít nhất 6 ký tự")]
+        [MaxLength(20, ErrorMessage = "Họ tên tối đa 20 ký tự")]
+        public string LdtFullName { get; set; }
+
+        [Display(Name = "Địa chỉ email")]
+        [Required(ErrorMessage = "Địa chỉ email không được để trống")]
+        [EmailAddress(ErrorMessage = "Địa chỉ email không đúng định dạng")]
+        public string LdtEmail { get; set; }
+
+        [Display(Name = "Số điện thoại")]
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression(@"^(\+?(?:[0-9]{3}))?[ .-]?(?:[0-9]{3})?[ .-]?(?:[0-9]{4}){1,2}$",
+            ErrorMessage = "Số điện thoại không đúng định dạng")]
+        [Required(ErrorMessage = "Số điện thoại không được để trống")]
+        public string LdtPhone { get; set; }
+
+        [Display(Name = "Địa chỉ thường trú")]
+        [Required(ErrorMessage = "Địa chỉ không được để trống")]
+        [StringLength(35, ErrorMessage = "Địa chỉ không vượt quá 35 ký tự")]
+        public string LdtAddress { get; set; }
+
+        [Display(Name = "Ảnh đại diện")]
+        public string LdtAvatar { get; set; }
+
+        [Display(Name = "Ngày sinh")]
+        [Required(ErrorMessage = "Ngày sinh không được để trống")]
+        [DataType(DataType.Date)]
+        public DateTime LdtBirthday { get; set; }
+
+        [Display(Name = "Giới tính")]
+        public string LdtGender { get; set; }
+
+        [Display(Name = "Mật khẩu")]
+        [Required(ErrorMessage = "Mật khẩu không được để trống")]
+        [MinLength(6, ErrorMessage = "Mật khẩu ít nhất 6 ký tự")]
+        public string LdtPassword { get; set; }
+
+        [Display(Name = "Facebook")]
+        public string LdtFacebook { get; set; }
+    }
+}
